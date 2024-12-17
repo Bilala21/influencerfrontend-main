4188084d844b0e29b7b8e2e0708a2633b344c8ba
Bilal <bilal.kalri@gmail>
2024-12-17 16:41:02 +0500

    changed

--- src/components/sellercomponents/selleradmin.jsx
+++ src/components/sellercomponents/selleradmin.jsx
@@ -208,10 +208,11 @@ export default function SellerAdminHeader({ children }) {
     };
 
     useEffect(() => {
+        handleResize();
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     
-    }, []);
+    },[]);
 
     return (
         <>
